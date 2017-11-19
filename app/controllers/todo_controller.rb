5c64b56a191cbd7f900a6dc11109fa98335a6ccb
Anthony Canas <roncanas1234@gmail.com>
2017-11-19 21:31:18 +0000

    edit curriculum

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,22 +1,3 @@
-
-class TodoController < ApplicationController
-  def index
-  end
-    def show
-  @todo = Todo.find_by_id(params[:id])
-   
-    end
-
-def new
-end
-def create
-  t = Todo.new
-  t.description = params['description']
-  t.pomodoro_estimate = params['pomodoro_estimate']
-  t.save
-  redirect_to "/todo/show/#{ t.id }"
-  end
-end
 class TodoController < ApplicationController
   def index
   end
@@ -25,11 +6,26 @@ class TodoController < ApplicationController
   end
   def new
   end
+  def edit
+  @todo = Todo.find_by_id(params[:id])
+  end
   def create
     t = Todo.new
     t.description = params['description']
     t.pomodoro_estimate = params['pomodoro_estimate']
     t.save
     redirect_to "/todo/show/#{ t.id }"
+  end
+  def update
+    t = Todo.find_by_id(params['id'])
+    t.description = params['description']
+    t.pomodoro_estimate = params['pomodoro-estimate']
+    t.save
+    redirect_to "/todo/show/#{t.id}"
+  end
+  def destroy
+    t = Todo.find_by_id(params[:id])
+    t.destroy
+    redirect_to "/todo/index"
   end
 end
